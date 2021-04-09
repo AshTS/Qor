0e710077d8d76603b40b5f192a5bbec66be08a44
CarterTS <carterplasek@gmail.com>
2021-04-09 01:41:35 -0500

    Red panic

--- qor-os/src/panic.rs
+++ qor-os/src/panic.rs
@@ -4,7 +4,7 @@ use crate::*;
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> !
 {
-    kprint!("Aborting: ");
+    kprint!("\x1B[31mAborting: ");
 
     if let Some(p) = info.location()
     {
@@ -15,7 +15,7 @@ fn panic(info: &core::panic::PanicInfo) -> !
         kprintln!("no info available");
     }
 
-    kprint!("");
+    kprint!("\x1B[m");
 
     abort();
 }
