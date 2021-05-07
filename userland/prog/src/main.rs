e2dfc340ba483d4a0a0417f87a7355c7c6b6b699
CarterTS <carterplasek@gmail.com>
2021-05-07 00:39:02 -0500

    Clean up slib

--- userland/prog/src/main.rs
+++ userland/prog/src/main.rs
@@ -1,18 +1,9 @@
 #![no_std]
 #![no_main]
 
-/// Panic handler for the kernel
-#[panic_handler]
-pub fn panic_handler(_info: &core::panic::PanicInfo) -> !
-{
-    loop {}
-}
-
-
 #[no_mangle]
 extern "C"
 fn _start()
 {
     slib::exit(5);
-    loop {}
 }
--- userland/slib/src/lib.rs
+++ userland/slib/src/lib.rs
@@ -4,7 +4,15 @@
 
 mod asm;
 
-pub fn exit(code: usize)
+/// Panic handler for the kernel
+#[panic_handler]
+pub fn panic_handler(_info: &core::panic::PanicInfo) -> !
+{
+    loop {}
+}
+
+pub fn exit(code: usize) -> !
 {
     unsafe { asm::make_syscall(60, code, 0, 0, 0, 0, 0) };
+    loop {}
 }
