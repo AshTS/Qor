226f35acd7f13be31d27b515684b5ebf6425c899
CarterTS <carterplasek@gmail.com>
2021-05-07 02:10:24 -0500

    Add losetup to build.sh

--- qor-os/build.sh
+++ qor-os/build.sh
@@ -6,11 +6,15 @@ cargo build
 
 cd ../../qor-os
 
-sudo mount /dev/loop11 /mnt
+sudo losetup /dev/loop11 hdd.dsk
 
+sudo mount /dev/loop11 /mnt
 sudo cp ../userland/prog/target/riscv64gc-unknown-none-elf/debug/prog /mnt/bin/prog
 
 stat /mnt/bin/prog
 
-sudo sync
+sudo sync /mnt
+
 sudo umount /mnt
+
+sudo losetup -d /dev/loop11
