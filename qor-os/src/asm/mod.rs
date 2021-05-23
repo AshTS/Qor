8c01a3f4f3bdc7366c935a9d799d156f6eb723f4
CarterTS <carterplasek@gmail.com>
2021-05-23 17:03:15 -0500

    Correct linker script to not overlap regions

--- qor-os/src/asm/mod.rs
+++ qor-os/src/asm/mod.rs
@@ -1,5 +1,5 @@
 //! Handles the assembly files
 
 global_asm!(include_str!("boot.s"));
-global_asm!(include_str!("mem.s"));
 global_asm!(include_str!("trap.s"));
+global_asm!(include_str!("mem.s"));
