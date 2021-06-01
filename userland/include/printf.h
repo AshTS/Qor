99f132caad9f8847236663b48a19fe7f365f528e
CarterTS <carterplasek@gmail.com>
2021-06-01 10:54:31 -0500

    Add single import checks to header files

--- userland/include/printf.h
+++ userland/include/printf.h
@@ -1,3 +1,8 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
 #include <stdarg.h>
 
 int printf(const char* data, ...);
+
+#endif // _PRINTF_H
--- userland/include/syscalls.h
+++ userland/include/syscalls.h
@@ -1,3 +1,6 @@
+#ifndef _SYSCALLS_H
+#define _SYSCALLS_H
+
 extern unsigned int write(int fd, void* buffer, int size);
 extern unsigned int open(const char* name, int mode);
 extern unsigned int close(int fd);
@@ -5,3 +8,5 @@ extern unsigned int read(int fd, void* buffer, int size);
 extern unsigned int fork();
 extern unsigned int execve(const char* path);
 extern unsigned int wait(int* wstatus);
+
+#endif // _SYSCALLS_H
