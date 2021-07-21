c2bfa16773bcf35500dd02c2889ebc8acdc318b1
CarterTS <carterplasek@gmail.com>
2021-07-21 18:55:35 -0500

    mkdir: Functional Implementation

--- userland/mkdir/src/main.c
+++ userland/mkdir/src/main.c
@@ -1,7 +1,19 @@
 #include "printf.h"
+#include "syscalls.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    printf("Hello World!\n");
+    if (argc < 2)
+    {
+        printf("mkdir requires atleast one argument\n");
+    }
+    else
+    {
+        if (mkdir(argv[1], 0x1FF) == -1)
+        {
+            printf("Unable to create directory `%s`\n", argv[1]);
+            return -1;
+        }
+    }
     return 0;
 }
