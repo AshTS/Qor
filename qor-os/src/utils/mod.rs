4d8a4f0444f35317fe2569b3f6128ba5d7d399d1
CarterTS <carterplasek@gmail.com>
2021-07-21 21:41:11 -0500

    Utils: Add seperate_path_last function

--- qor-os/src/utils/mod.rs
+++ qor-os/src/utils/mod.rs
@@ -6,3 +6,18 @@ pub use memdump::*;
 
 pub mod ringbuffer;
 pub use ringbuffer::*;
+
+use crate::*;
+
+/// Seperate a path into a path and the last item (path, name)
+pub fn separate_path_last(path: &str) -> (String, String)
+{
+    if let Some((name, path_items)) = path.split("/").collect::<Vec<_>>().split_last()
+    {
+        (path_items.join("/") + "/", name.to_string())
+    }
+    else
+    {
+        (String::new(), String::new())
+    }
+}
