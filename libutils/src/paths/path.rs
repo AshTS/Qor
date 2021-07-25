3a413b50b281b67fd549908efa371ea3e8228626
CarterTS <carterplasek@gmail.com>
2021-07-25 16:40:42 -0500

    libutils: Add PathIterator

--- libutils/src/paths/path.rs
+++ libutils/src/paths/path.rs
@@ -14,20 +14,97 @@ impl OwnedPath
     {
         Self
         {
-            path: data.into()
+            path: data.into(),
+        }
+    }
+
+    /// Convert the path to an iter
+    pub fn iter(&self) -> PathIterator
+    {
+        PathIterator
+        {
+            path: &self,
+            iter_index: None,
         }
     }
 }
 
-/// Path Buffer Object
-type PathBuffer<'a> = &'a OwnedPath;
+impl core::fmt::Display for OwnedPath
+{
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result
+    {
+        write!(f, "{}", self.path)
+    }
+}
 
-impl<'a> core::iter::Iterator for PathBuffer<'a>
+/// Path Buffer Object
+pub type PathBuffer<'a> = &'a OwnedPath;
+
+/// Path Iterator
+///
+/// Iterates over the directory names in a path, for example, converting the
+/// path `/usr/bin/ls` to an iterator will yield `&str`'s with the same lifetime
+/// as the reference to the path, in the following order: `{"usr", "bin", "ls"}`
+pub struct PathIterator<'a>
+{
+    path: PathBuffer<'a>,
+    iter_index: Option<usize>
+}
+
+impl<'a> core::iter::Iterator for PathIterator<'a>
 {
     type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item>
     {
-        todo!()
+        // Initialize the iterator to start at the begining of the path
+        if self.iter_index.is_none()
+        {
+            self.iter_index = Some(0);
+        }
+
+        if let Some(mut start_index) = self.iter_index
+        {
+            // If the starting index places us at a '/', step one character
+            // forwards
+            if self.path.path.chars().nth(start_index) == Some('/')
+            {
+                start_index += 1;
+            }
+
+            // Ensure we are still within the size of the path
+            if start_index < self.path.path.len()
+            {
+                let mut end_index = start_index + 1;
+
+                while end_index < self.path.path.len() && self.path.path.chars().nth(end_index) != Some('/')
+                {
+                    end_index += 1;
+                }
+
+                self.iter_index = Some(end_index);
+
+                // If we ran past the end of the path, get the slice to the end
+                // of the path
+                if end_index >= self.path.path.len()
+                {
+                    Some(&self.path.path[start_index..])
+                }
+                // Otherwise, get the slice to before the '/'
+                else
+                {
+                    Some(&self.path.path[start_index..end_index])
+                }
+            }
+            // If not, return None, as we are done iterating
+            else
+            {
+                None 
+            }
+        }
+        else
+        {
+            unreachable!()
+        }
     }
 }
--- libutils/tests/paths.rs
+++ libutils/tests/paths.rs
@@ -1,1 +1,23 @@
 extern crate libutils;
+
+/// Test the Path Iterator
+#[test]
+pub fn test_path_iterator()
+{
+    use libutils::paths::OwnedPath;
+
+    let path0 = OwnedPath::new("/usr/bin/ls");
+    let path1 = OwnedPath::new("bin/ls");
+    let path2 = OwnedPath::new("/");
+    let path3 = OwnedPath::new("./../../home/");
+
+    assert_eq!(path0.iter().collect::<Vec<_>>(), vec!["usr", "bin", "ls"]);
+    assert_eq!(path1.iter().collect::<Vec<_>>(), vec!["bin", "ls"]);
+    assert_eq!(path2.iter().collect::<Vec<_>>(), Vec::<&str>::new());
+    assert_eq!(path3.iter().collect::<Vec<_>>(), vec![".", "..", "..", "home"]);
+
+    assert_eq!((&path0).iter().collect::<Vec<_>>(), vec!["usr", "bin", "ls"]);
+    assert_eq!((&path1).iter().collect::<Vec<_>>(), vec!["bin", "ls"]);
+    assert_eq!((&path2).iter().collect::<Vec<_>>(), Vec::<&str>::new());
+    assert_eq!((&path3).iter().collect::<Vec<_>>(), vec![".", "..", "..", "home"]);
+}
