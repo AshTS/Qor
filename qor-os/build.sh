e96af556a4ff28b1d3e79822288c3fb4a56ec636
CarterTS <carterplasek@gmail.com>
2021-07-31 16:51:51 -0500

    ps: Add empty executable

--- qor-os/build.sh
+++ qor-os/build.sh
@@ -3,7 +3,7 @@ if not test -d ../userland/bin
   mkdir ../userland/bin
 end
 
-set programs slib libc term shell prog hello libc-test pwd basic cat ls clear mkdir checkers bmp
+set programs slib libc term shell prog hello libc-test pwd basic cat ls clear mkdir checkers bmp ps
 
 cd ../userland
 
