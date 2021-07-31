c3245c734558ad4f01b8ce1587fec3b10f3925ea
CarterTS <carterplasek@gmail.com>
2021-07-31 17:08:07 -0500

    LibC: Add sprintf

--- userland/include/printf.h
+++ userland/include/printf.h
@@ -2,8 +2,10 @@
 #define _PRINTF_H
 
 #include <stdarg.h>
+#include "syscalls.h"
 
 extern int fprintf(int fd, const char* data, ...);
+extern int sprintf(char* dest, const char* data, ...);
 
 #define printf(...) fprintf (1, __VA_ARGS__)
 #define eprintf(...) fprintf (2, __VA_ARGS__)
--- userland/libc/src/printf.c
+++ userland/libc/src/printf.c
@@ -178,3 +178,176 @@ int fprintf(int fd, const char* data, ...)
 
     return index;
 }
+
+// Sput implementation for sprintf
+void local_sput(const char* data, char** dest)
+{
+    int count = 0;
+    while (*(data + count))
+    {
+        *((*(dest))++) = *(data + count);
+        count++;
+    }
+}
+
+// Helper function for sprintf
+void sprintf_helper(char* buffer, unsigned int* index, char c, char** dest)
+{
+    buffer[((*index)++) % (PRINTF_BUFFER_LEN - 1)] = c;
+    if (*index % (PRINTF_BUFFER_LEN - 1) == 0 || c == 0)
+    {
+        local_sput(buffer, dest);
+    }
+}
+
+// String Printf
+int sprintf(char* dest, const char* data, ...)
+{
+    va_list args;
+    va_start(args, data);
+
+    // Initialize the buffer
+    char buffer[PRINTF_BUFFER_LEN];
+    unsigned int index = 0;
+
+    for (int i = 0; i < PRINTF_BUFFER_LEN; i++)
+    {
+        buffer[i] = 0;
+    }
+
+    // Flag for if the next character is to be a format specifier
+    bool next_format_specifier = false;
+
+    // Last character (for two byte format specifiers)
+    char last_char = 0;
+
+    // Loop over every character in the input stream
+    char c = 1;
+    while (c)
+    {
+        // Get the next character
+        c = *(data++);
+
+        if (!next_format_specifier)
+        {   
+            if (c == '%')
+            {
+                next_format_specifier = true;
+                last_char = c;
+                continue;
+            }
+
+            last_char = c;
+        }
+        else
+        {
+            if (c == 'i')
+            {
+                int i = va_arg(args, int);
+
+                if (i < 0)
+                {
+                    i = -i;
+                    sprintf_helper(buffer, &index, '-', &dest);
+                }
+
+                int counter = 1;
+                while (counter <= i / 10)
+                {
+                    counter *= 10;
+                }
+
+                while (counter >= 1)
+                {
+                    sprintf_helper(buffer, &index, '0' + (i / counter) % 10, &dest);
+                    counter /= 10;
+                }
+
+                next_format_specifier = false;
+            }
+            else if (c == 'l')
+            {
+                next_format_specifier = true;
+            }
+            else if (c == 'd' && last_char == 'l')
+            {
+                long i = va_arg(args, long);
+
+                if (i < 0)
+                {
+                    i = -i;
+                    sprintf_helper(buffer, &index, '-', &dest);
+                }
+
+                long counter = 1;
+                while (counter <= i / 10)
+                {
+                    counter *= 10;
+                }
+
+                while (counter >= 1)
+                {
+                    sprintf_helper(buffer, &index, '0' + (i / counter) % 10, &dest);
+                    counter /= 10;
+                }
+
+                next_format_specifier = false;
+            }
+            else if (c == 'p')
+            {
+                unsigned long i = va_arg(args, unsigned long);
+
+                sprintf_helper(buffer, &index, '0', &dest);
+                sprintf_helper(buffer, &index, 'x', &dest);
+
+                unsigned long counter = 1;
+                while (counter <= i / 16)
+                {
+                    counter *= 16;
+                }
+
+                while (counter >= 1)
+                {
+                    char digit = (i / counter) % 16;
+
+                    if (digit < 10)
+                    {
+                        sprintf_helper(buffer, &index, '0' + digit, &dest);
+                    }
+                    else
+                    {
+                        sprintf_helper(buffer, &index, 'A' + (digit - 10), &dest);
+                    }
+                    
+                    counter /= 16;
+                }
+                next_format_specifier = false;
+            }
+            else if (c == 's')
+            {
+                const char* s = va_arg(args, const char*);
+
+                while (*s)
+                {
+                    sprintf_helper(buffer, &index, *(s++), &dest);
+                }
+                next_format_specifier = false;
+            }
+            else if (c == 'c')
+            {
+                int c = va_arg(args, int);
+
+                sprintf_helper(buffer, &index, (char)c, &dest);
+                next_format_specifier = false;
+            }
+            last_char = c;
+            continue;
+        }
+        
+
+        // Add to the buffer and possibly refresh the buffer
+        sprintf_helper(buffer, &index, c, &dest);
+    }
+
+    return index;
+}
