9f346f199dea5532b69a639121a485ef0f58efcb
CarterTS <carterplasek@gmail.com>
2021-07-31 14:51:12 -0500

    Terminal: Add terminal program

--- qor-os/build.sh
+++ qor-os/build.sh
@@ -3,7 +3,7 @@ if not test -d ../userland/bin
   mkdir ../userland/bin
 end
 
-set programs slib shell prog hello libc-test pwd basic cat ls clear mkdir checkers bmp
+set programs slib libc term shell prog hello libc-test pwd basic cat ls clear mkdir checkers bmp
 
 cd ../userland
 
