cbd99aea27b0f978211198193c2e65f5d9e234e2
CarterTS <carterplasek@gmail.com>
2021-08-01 12:37:55 -0500

    ps: Show memory usage

--- userland/ps/src/main.c
+++ userland/ps/src/main.c
@@ -12,6 +12,8 @@ struct linux_dirent
    char           d_name[];
 };
 
+int string_to_int(char* s);
+void display_pid(int pid);
 
 int main()
 {
@@ -51,38 +53,13 @@ int main()
                     continue;
                 }
 
+                int pid = string_to_int((char*)d->d_name);
+
                 char path[256];
 
                 if (d->d_ino != 0)
                 {
-                    sprintf(path, "/proc/%s/cmdline", (char *)d->d_name);
-                    int fd = open(path, O_RDONLY);
-
-                    if (fd < 0)
-                    {
-                        printf("Unable to open `%s`\n", path);
-                        continue;
-                    }
-
-                    int length = strlen((char *)d->d_name);
-
-                    printf("%s", (char *)d->d_name);
-
-                    for (int i = 0; i < (5 - length); i++)
-                    {
-                        printf(" ");
-                    }
-
-                    int l = read(fd, path, 256);
-                    
-                    for (int i = 0; i < l; i++)
-                    {
-                        printf("%c", path[i] ? path[i] : ' ');
-                    }
-
-                    printf("\n");
-
-                    close(fd);
+                    display_pid(pid);
                 } 
             }
 
@@ -91,4 +68,97 @@ int main()
     }   
 
     return 0;
+}
+
+
+int string_to_int(char* s)
+{
+    int result = 0;
+    while (*s >= '0' && *s <= '9')
+    {
+        result *= 10;
+        result += *s - '0';
+        s++;
+    }
+
+    return result;
+}
+
+int get_mem_usage(int pid)
+{
+    char path[256];
+
+    sprintf(path, "/proc/%i/statm", pid);
+    int fd = open(path, O_RDONLY);
+
+    if (fd < 0)
+    {
+        // printf("\nUnable to open `%s`\n", path);
+        return -1;
+    }
+
+    int l = read(fd, path, 255);
+    path[l] = 0;
+
+    close(fd);
+
+    return string_to_int(path);
+}
+
+int render_size(int size)
+{
+    char endings[] = {'K', 'M', 'G', 'T'};
+    char i = 0;
+
+    while (size > 2048)
+    {
+        size /= 1024;
+        i++;
+    }
+
+    return printf("%i%c", size, endings[i]);
+}
+
+void display_pid(int pid)
+{
+    char path[256];
+
+    sprintf(path, "/proc/%i/cmdline", pid);
+    int fd = open(path, O_RDONLY);
+
+    if (fd < 0)
+    {
+        printf("Unable to open `%s`\n", path);
+        return;
+    }
+
+    int length = printf("%i", pid);
+
+    for (int i = 0; i < (3 - length); i++)
+    {
+        length += printf(" ");
+    }
+
+    int l = read(fd, path, 256);
+    for (int i = 0; i < l; i++)
+    {
+        if (path[i] == 0) { break; }
+        printf("%c", path[i]);
+        length += 1;
+    }
+
+    for (int i = 0; i < (12 - length); i++)
+    {
+        length += printf(" ");
+    }
+
+    int mem_usage = get_mem_usage(pid) * 4;
+
+    length += render_size(mem_usage);
+
+
+
+    printf("\n");
+
+    close(fd);
 }
