50a79fdb66f469d61de1b54481059e860a3751bb
CarterTS <carterplasek@gmail.com>
2021-10-19 18:36:27 -0500

    Kernel: Add userspace floating point support

--- qor-os/src/asm/trap.s
+++ qor-os/src/asm/trap.s
@@ -41,6 +41,19 @@ asm_trap_vector:
     csrr t6, mscratch
     save_gp 31, t5
 
+    csrr t1, mstatus
+    srli t0, t1, 13
+    andi t0, t0, 3
+    li t3, 3
+    bne t0, t3, skip_float_save
+    .set i, 0
+    .rept 32
+        save_fp %i, t5
+        .set i, i+1
+    .endr
+
+skip_float_save:
+
     csrw mscratch, t5
 
     # Set up the arguments for the m_trap function
@@ -71,7 +84,7 @@ asm_trap_vector:
 .global switch_to_user
 switch_to_user:
     csrw mscratch, a0
-    li t0, (1 << 7) | (1 << 5)
+    li t0, (1 << 7) | (1 << 5) | (1 << 13)
     csrw mstatus, t0
     csrw mepc, a1
     csrw satp, a2
@@ -83,8 +96,14 @@ switch_to_user:
     csrw mtvec, t2
 
     sfence.vma
-
     mv t6, a0
+
+    .set i, 0
+    .rept 32
+        load_fp %i
+        .set i, i + 1
+    .endr
+
     .set i, 1
     .rept 31
         load_gp %i, t6
