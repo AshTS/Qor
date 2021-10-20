6bab128bc0a43a00c6375b5ca770893e40036f0b
CarterTS <carterplasek@gmail.com>
2021-10-20 01:35:10 -0500

    fractal: Switch the fractal plotter to libgraphics

--- userland/fractal/src/main.c
+++ userland/fractal/src/main.c
@@ -1,115 +1,195 @@
 #include "printf.h"
+#include "libgraphics.h"
+#include "complex.h"
 
-#define PREC 10000
-#define ZOOM 1
-#define CENTER_X -7500	// -0.75
-#define CENTER_Y -0   // 0.0
+#define CENTER (struct Complex){.real = -0.75, .imag = 0.0}
 
-struct Pixel
+void libgraphics_error();
+struct Pixel shader(int x, int y, int m_or_n);
+
+struct Complex point_to_complex(int x, int y, struct Complex center);
+
+struct Pixel mandelbrot(struct Complex c);
+struct Pixel newton(struct Complex c, 
+                    struct Complex (func)(struct Complex), 
+                    struct Complex (deriv)(struct Complex), 
+                    struct Complex zeros[], int zero_count,
+                    int iterations);
+
+void help();
+
+int main(int argc, char** argv)
 {
-    char r;
-    char g;
-    char b;
-    char a;
-};
-
-struct Complex
-{
-    float real;
-    float imag;
-};
-
-char check_pixel(int x, int y, int zoom);
-
-struct Complex iterate(struct Complex z, struct Complex c);
-
-float mag2(struct Complex z);
-
-int main()
-{
-    printf("Fractal Plotter!\n");
-
-    int fb_fd = open("/dev/fb0", O_WRONLY);
-
-    if (fb_fd == -1)
+    if (argc < 2 || argv[1][0] != '-' || (argv[1][1] != 'm' && argv[1][1] != 'n') || argv[1][2] != 0)
     {
-        eprintf("Unable to open /dev/fb0\n");
+        help();
+
         return -1;
     }
-    struct Pixel* framebuffer = mmap(0, 640 * 480 * 4, PROT_READ | PROT_WRITE, 0, fb_fd, 0);
 
-    struct Pixel color;
+    int m_or_n = argv[1][1] == 'm';
 
-    color.r = 255;
-    color.g = 255;
-    color.b = 255;
-    color.a = 255;
+    // Attempt to initialize the framebuffer, otherwise display the error and
+    // return
+    if (init_framebuffer() < 0)
+    {
+        libgraphics_error();
+        return -1;
+    }
 
+    // Attempt to get the framebuffer
+    struct Pixel* framebuffer = get_framebuffer();
+
+    // Make sure the framebuffer exists, otherwise display the error and return
+    if (framebuffer == 0)
+    {
+        libgraphics_error();
+        return -1;
+    }
+
+    // Loop over every pixel and request its color from the 'shader'
     for (int x = 0; x < 640; x++)
     {
         for (int y = 0; y < 480; y++)
         {
-            char c = check_pixel(x, y, ZOOM);
-
-            color.r = c;
-            color.g = c;
-            color.b = c;
-
-            framebuffer[x + 640 * y] = color;
+            framebuffer[compute_location(x, y)] = shader(x, y, m_or_n);
         }
     }
 
-    munmap(framebuffer, 640 * 480 * 4);
-
-    close(fb_fd);
+    // Attempt to close the framebuffer, otherwise display the error and return
+    if (close_framebuffer() < 0)
+    {
+        libgraphics_error();
+        return -1;
+    }
 
     return 0;
 }
 
-char check_pixel(int x, int y, int zoom)
+// Display the error message from the libgraphics library
+void libgraphics_error()
 {
-    struct Complex z;
+    eprintf("libgraphics error: ");
 
-    z.real = 0.0;
-    z.imag = 0.0;
+    if (LIBGRAPHICS_ERROR == LIBGRAPHICS_UNABLE_TO_MAP_FRAMEBUFFER)
+    {
+        eprintf("UNABLE_TO_MAP_FRAMEBUFFER\n");
+    }
+    else if (LIBGRAPHICS_ERROR == LIBGRAPHICS_UNABLE_TO_OPEN_FRAMEBUFFER)
+    {
+        eprintf("UNABLE_TO_OPEN_FRAMEBUFFER\n");
+    }
+    else if (LIBGRAPHICS_ERROR == LIBGRAPHICS_UNINITIALIZED_FRAMEBUFFER)
+    {
+        eprintf("UNINITIALIZED_FRAMEBUFFER\n");
+    }
+    else if (LIBGRAPHICS_ERROR == LIBGRAPHICS_UNMAPPED_FRAMEBUFFER)
+    {
+        eprintf("UNMAPPED_FRAMEBUFFER\n");
+    }
+}
 
-    struct Complex c;
+// f(z) = z^3 - 1
+struct Complex f(struct Complex z)
+{
+    return sub(mult(z, mult(z, z)), (struct Complex){.real = 1.0, .imag = 0.0});
+}
 
-    c.real = (x - 320) / 200.0 / zoom - 0.75;
-    c.imag = (y - 240) / 200.0 / zoom;
+// f'(z) = 3z^2
+struct Complex d(struct Complex z)
+{
+    return scale(mult(z, z), 3);
+}
+
+// Shader function, maps an (x, y) pair to a color
+struct Pixel shader(int x, int y, int m_or_n)
+{
+    struct Complex c = point_to_complex(x, y, CENTER);
+    
+    if (m_or_n)
+    {
+        return mandelbrot(c);
+    }
+    else
+    {
+        float sqrt3d2 = 0.86602540378;
+
+        struct Complex zeros[3] = {(struct Complex){.real = 1.0, .imag = 0.0},
+                                (struct Complex){.real = -0.5, .imag = sqrt3d2},
+                                (struct Complex){.real = -0.5, .imag = -sqrt3d2}};
+
+        return newton(c, f, d, zeros, 3, 10);
+    }
+}
+
+// Convert an (x, y) point to a complex number for the shader
+struct Complex point_to_complex(int x, int y, struct Complex center)
+{
+    return add(center, (struct Complex){.real = (float)(x - 320) / 200.0, .imag = (float)(y - 240) / 200.0});
+}
+
+// Mandelbrot set shader
+struct Pixel mandelbrot(struct Complex c)
+{
+    struct Complex z = (struct Complex){.real = 0.0, .imag = 0.0};
 
     char val = 255;
 
-    while (val != 0)
+    while (val > 0)
     {
-        z = iterate(z, c);
-
-        if (mag2(z) > 4.0)
+        if (abs2(z) > 4.0)
         {
             break;
         }
 
+        z = add(mult(z, z), c);
         val -= 1;
     }
 
-    return val;
+    return (struct Pixel){.r = val, .g = val, .b = val, .a = 255};
 }
 
-struct Complex iterate(struct Complex z, struct Complex c)
+// Newton Fractal
+struct Pixel newton(struct Complex c, 
+                    struct Complex (func)(struct Complex), 
+                    struct Complex (deriv)(struct Complex), 
+                    struct Complex zeros[], int zero_count,
+                    int iterations)
 {
-    struct Complex sqr;
+    static struct Pixel colors[6] = {(struct Pixel){.r = 255, .g = 128, .b = 128, .a = 255}, 
+                                     (struct Pixel){.r = 128, .g = 255, .b = 128, .a = 255}, 
+                                     (struct Pixel){.r = 128, .g = 128, .b = 255, .a = 255}, 
+                                     (struct Pixel){.r = 255, .g = 255, .b = 128, .a = 255}, 
+                                     (struct Pixel){.r = 128, .g = 255, .b = 255, .a = 255}, 
+                                     (struct Pixel){.r = 255, .g = 128, .b = 255, .a = 255}};
 
-    sqr.real = (z.real * z.real - z.imag * z.imag);
-    sqr.imag = (z.real * z.imag * 2);
+    for (int i = 0; i < iterations; i++)
+    {
+        c = sub(c, div(func(c), deriv(c)));
+    }
 
-    sqr.real += c.real;
-    sqr.imag += c.imag;
+    float best_dist = abs2(sub(c, zeros[0]));
+    int best = 0;
 
-    return sqr;
+    for (int i = 1; i < zero_count; i++)
+    {
+        float dist = abs2(sub(c, zeros[i]));
+
+        if (dist < best_dist)
+        {
+            best_dist = dist;
+            best = i;
+        }
+    }
+
+    return colors[best];
 }
 
-
-float mag2(struct Complex z)
+// Display the help
+void help()
 {
-    return (z.imag * z.imag + z.real * z.real);
+    printf("Usage: fractal [OPTIONS]\n  Displays fractals using the libgraphics API\n\n");
+    
+    printf("  -m       Display the Mandelbrot set\n");
+    printf("  -n       Display the Newton fractal for f(z) = z^3 - 1\n");
 }
