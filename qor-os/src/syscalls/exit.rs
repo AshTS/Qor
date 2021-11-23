8dd9942180315bcc89f6f63abfc920ed6e9428ac
CarterTS <carterplasek@gmail.com>
2021-11-22 18:19:37 -0600

    Kernel: Fix exit returning

--- qor-os/src/syscalls/exit.rs
+++ qor-os/src/syscalls/exit.rs
@@ -6,4 +6,7 @@ pub fn syscall_exit(proc: &mut super::Process, value: usize)
     kdebugln!(Syscalls, "Exiting Process PID {} with value: {}", proc.pid, value);
 
     proc.kill(value);
+
+    let schedule = process::scheduler::schedule_next();
+    process::scheduler::schedule_jump(schedule);
 }
