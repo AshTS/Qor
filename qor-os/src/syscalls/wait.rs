eb0a4fa4e62ad0f459f926eecaee9c2dc263fe5c
CarterTS <carterplasek@gmail.com>
2022-02-17 21:37:42 -0600

    syscalls: wait returns an error with no children

--- qor-os/src/syscalls/wait.rs
+++ qor-os/src/syscalls/wait.rs
@@ -13,6 +13,11 @@ pub fn syscall_wait(proc: &mut super::Process, ptr: usize) -> usize
             0 as *mut u32
         };
 
+    if proc.data.children.len() == 0
+    {
+        return errno::ECHILD;
+    }
+
     proc.state = process::process::ProcessState::Waiting(process::process::WaitMode::ForChild);
     proc.data.return_code_listener = unsafe { status.as_mut() };
     proc.program_counter += 4;
