cca8670d166a8b233a6bdf956313df8fcac152ab
CarterTS <carterplasek@gmail.com>
2022-11-06 16:25:31 -0600

    Mem: Add formatting for memory units

--- qor-os/src/mem/units.rs
+++ qor-os/src/mem/units.rs
@@ -1,5 +1,5 @@
 /// Memory Unit Generic Type, stores the size of a memory region as a number of a particular size of units
-pub struct MemoryUnit<const Scale: usize>(usize);
+pub struct MemoryUnit<const SCALE: usize>(usize);
 
 /// Memory Unit for Pages
 pub type PageCount = MemoryUnit<{super::PAGE_SIZE}>;
@@ -10,7 +10,7 @@ pub type KiByteCount = MemoryUnit<1024>;
 /// Memory Unit for Bytes
 pub type ByteCount = MemoryUnit<1>;
 
-impl<const Scale: usize> MemoryUnit<Scale> {
+impl<const SCALE: usize> MemoryUnit<SCALE> {
     /// Construct the type from a particular number of units
     pub const fn new(units: usize) -> Self {
         Self(units)
@@ -23,7 +23,7 @@ impl<const Scale: usize> MemoryUnit<Scale> {
 
     /// Get the raw number of bytes
     pub const fn raw_bytes(&self) -> usize {
-        Scale * self.0
+        SCALE * self.0
     }
 
     /// Get a mutable reference to the number of bytes
@@ -32,8 +32,26 @@ impl<const Scale: usize> MemoryUnit<Scale> {
     }
 }
 
-impl<const ScaleSrc: usize> MemoryUnit<ScaleSrc> {
-    pub const fn convert<const ScaleDest: usize>(&self) -> MemoryUnit<ScaleDest> {
-        MemoryUnit((self.raw_bytes() + ScaleDest - 1) / ScaleDest)
+impl<const SRC: usize> MemoryUnit<SRC> {
+    pub const fn convert<const DEST: usize>(&self) -> MemoryUnit<DEST> {
+        MemoryUnit((self.raw_bytes() + DEST - 1) / DEST)
+    }
+}
+
+impl core::fmt::Display for MemoryUnit<1> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{} B", self.0)
+    }
+}
+
+impl core::fmt::Display for MemoryUnit<1024> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{} KiB", self.0)
+    }
+}
+
+impl core::fmt::Display for MemoryUnit<{super::PAGE_SIZE}> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{} Pages ({} KiB)", self.0, self.convert::<1024>().raw())
     }
 }
